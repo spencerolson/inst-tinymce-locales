02ca918d2f29434d966e3c184245a47449214651
Spencer Olson <solson@instructure.com>
2019-07-08 13:44:06 -0500

    remove spread syntax

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const locales = require('./lib/locales/index')
 
-const defaultLanguageCodeMapings = {
-  ar: 'ar_SA',
-  da: 'da',
-  de: 'de',
-  'en-au-x-unimelb': 'en_GB',
-  en_AU: 'en_GB',
-  en_GB: 'en_GB',
-  es: 'es',
-  fr: 'fr_FR',
-  fr_CA: 'fr_FR',
-  ja: 'ja',
-  mi: undefined, // no tinymce translations yet, will use english
-  nb: 'nb_NO',
-  nl: 'nl',
-  pl: 'pl',
-  pt: 'pt_PT',
-  pt_BR: 'pt_BR',
-  ru: 'ru',
-  zh: 'zh_CN',
-  zh_HK: 'zh_TW',
-}
+const defaultLanguageCodeMappings = () => (
+  {
+    ar: 'ar_SA',
+    da: 'da',
+    de: 'de',
+    'en-au-x-unimelb': 'en_GB',
+    en_AU: 'en_GB',
+    en_GB: 'en_GB',
+    es: 'es',
+    fr: 'fr_FR',
+    fr_CA: 'fr_FR',
+    ja: 'ja',
+    mi: undefined, // no tinymce translations yet, will use english
+    nb: 'nb_NO',
+    nl: 'nl',
+    pl: 'pl',
+    pt: 'pt_PT',
+    pt_BR: 'pt_BR',
+    ru: 'ru',
+    zh: 'zh_CN',
+    zh_HK: 'zh_TW',
+  }
+)
 
-exports.fetchTranslations = (languageCode, customLanguageCodeMappings) => {
-  const mappings = { ...defaultLanguageCodeMapings, ...customLanguageCodeMappings }
+exports.fetchTranslations = (languageCode, customLanguageCodeMappings = {}) => {
+  const mappings = defaultLanguageCodeMappings()
+  Object.keys(customLanguageCodeMappings).forEach(key => { mappings[key] = customLanguageCodeMappings[key] })
   const tinyMCELanguageCode = mappings[languageCode]
   const content = tinyMCELanguageCode && locales[tinyMCELanguageCode]
   return tinyMCELanguageCode && content && { languageCode: tinyMCELanguageCode, content }
