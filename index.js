72f61d7142daaf3a3a50f4c606bf5ff2c9f726e0
Spencer Olson <solson@instructure.com>
2019-07-08 11:12:20 -0500

    add es

---
+++ index.js
@@ -0,0 +1,38 @@
+import locales from 'lib/locales'
+
+const tinyMCELocales = {
+  es: 'es'
+}
+
+const instLocales = [
+  'en',
+  'en-au-x-unimelb',
+  'zh_HK',
+  'nb',
+  'en_GB',
+  'mi',
+  'es',
+  'da',
+  'pt',
+  'de',
+  'en_AU',
+  'pl',
+  'sv',
+  'fr_CA',
+  'ja',
+  'pt_BR',
+  'ru',
+  'fr',
+  'ar',
+  'nl',
+  'zh',
+]
+
+exports.fetchLocale = (originLocale) => {
+  const locale = tinyMCELocales[originLocale]
+  if (!locale) {
+    return locales.en
+  }
+
+  return locales[locale]
+}
